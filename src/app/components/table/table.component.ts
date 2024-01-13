674103700cbf53e9df1b05bbe9ab8501e49131bc
Ljusaj123 <rosana.ljusaj2@gmail.com>
2024-01-13 10:42:06 +0100

    added filtering and created button for new user

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -39,4 +39,9 @@ export class TableComponent {
       this.dataSource.sort = this.sort;
     });
   }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 }
