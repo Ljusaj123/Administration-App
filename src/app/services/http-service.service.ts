5cf03da4b428d941ff0bdb48624549704b467442
Ljusaj123 <rosana.ljusaj2@gmail.com>
2024-01-13 10:22:39 +0100

    created http service and API for users

---
+++ src/app/services/http-service.service.ts
@@ -0,0 +1,73 @@
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { OAuthService } from "angular-oauth2-oidc";
+import { Observable } from "rxjs";
+
+@Injectable({
+  providedIn: "root",
+})
+export class HttpService {
+  constructor(private http: HttpClient, private oauthService: OAuthService) {}
+
+  baseUrl: string =
+    "https://keycloak-tt-keycloak-stage.apps.ocp.thingstalk.eu/admin/realms/test-realm1/users/";
+
+  getAllUsers(): Observable<any> {
+    let headers = new HttpHeaders().set(
+      "Authorization",
+      "Bearer " + sessionStorage.getItem("access_token")
+    );
+
+    return this.http.get(this.baseUrl, {
+      headers: headers,
+    });
+  }
+
+  getSingleUser(id: string): Observable<any> {
+    let headers = new HttpHeaders().set(
+      "Authorization",
+      "Bearer " + sessionStorage.getItem("access_token")
+    );
+
+    return this.http.get(this.baseUrl + id, {
+      headers: headers,
+    });
+  }
+
+  deleteUser(id: string): Observable<any> {
+    let headers = new HttpHeaders().set(
+      "Authorization",
+      "Bearer " + sessionStorage.getItem("access_token")
+    );
+
+    return this.http.delete(this.baseUrl + id, {
+      headers: headers,
+    });
+  }
+
+  updateUser(user: any): Observable<any> {
+    let headers = new HttpHeaders().set(
+      "Authorization",
+      "Bearer " + sessionStorage.getItem("access_token")
+    );
+
+    const body = user;
+
+    return this.http.put(this.baseUrl + user.id, body, {
+      headers: headers,
+    });
+  }
+
+  createUser(user: any): Observable<any> {
+    let headers = new HttpHeaders().set(
+      "Authorization",
+      "Bearer " + sessionStorage.getItem("access_token")
+    );
+
+    const body = user;
+
+    return this.http.post(this.baseUrl, body, {
+      headers: headers,
+    });
+  }
+}
