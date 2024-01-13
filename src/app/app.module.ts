7d3c283c604ce18abaac9b6d79242807c6bddc9d
Ljusaj123 <rosana.ljusaj2@gmail.com>
2024-01-13 09:59:01 +0100

    implemented authentication

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,28 @@ import { BrowserModule } from "@angular/platform-browser";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { WelcomeComponent } from './pages/welcome/welcome.component';
-import { HomeComponent } from './pages/home/home.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
+import { WelcomeComponent } from "./pages/welcome/welcome.component";
+import { HomeComponent } from "./pages/home/home.component";
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { HttpClientModule } from "@angular/common/http";
+import { RouterOutlet } from "@angular/router";
+import { OAuthModule } from "angular-oauth2-oidc";
 
 @NgModule({
-  declarations: [AppComponent, WelcomeComponent, HomeComponent, NavbarComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  declarations: [
+    AppComponent,
+    WelcomeComponent,
+    HomeComponent,
+    NavbarComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    RouterOutlet,
+    OAuthModule.forRoot(),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,8 +1,41 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { OAuthService, UrlHelperService } from "angular-oauth2-oidc";
+import { authCodeFlowConfig } from "../../auth-config";
+import { JwksValidationHandler } from "angular-oauth2-oidc-jwks";
 
 @Component({
   selector: "app-navbar",
   templateUrl: "./navbar.component.html",
   styleUrl: "./navbar.component.css",
+  providers: [OAuthService, UrlHelperService],
 })
-export class NavbarComponent {}
+export class NavbarComponent implements OnInit {
+  name: string = "";
+
+  constructor(private oauthService: OAuthService) {
+    this.oauthService.setStorage(sessionStorage);
+  }
+
+  ngOnInit(): void {
+    this.consfigureSingleSignOn();
+  }
+
+  consfigureSingleSignOn() {
+    this.oauthService.configure(authCodeFlowConfig);
+    this.oauthService.tokenValidationHandler = new JwksValidationHandler();
+    this.oauthService.loadDiscoveryDocumentAndTryLogin();
+  }
+
+  login() {
+    this.oauthService.initCodeFlow();
+  }
+
+  logout() {
+    this.oauthService.logOut(true);
+  }
+
+  get token() {
+    const claims = this.oauthService.getIdentityClaims();
+    return claims ?? null;
+  }
+}
